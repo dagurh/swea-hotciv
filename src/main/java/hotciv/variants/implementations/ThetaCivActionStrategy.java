00c5c41a24a03ee1e9ea9f39561b4e192084fa75
Jonas <jonasvilla34@gmail.com>
2020-10-30 14:23:22 +0100

    ThetaCivActionStrategy decorator

--- src/main/java/hotciv/variants/implementations/ThetaCivActionStrategy.java
+++ src/main/java/hotciv/variants/implementations/ThetaCivActionStrategy.java
@@ -7,11 +7,21 @@ import hotciv.standard.GameImpl;
 import hotciv.variants.interfaces.ActionStrategy;
 
 public class ThetaCivActionStrategy implements ActionStrategy {
+
+    private GammaCivActionStrategy gammaCivActionStrategy;
+
+    public ThetaCivActionStrategy() {
+        this.gammaCivActionStrategy = new GammaCivActionStrategy();
+    }
+
+
     @Override
     public void unitAction(GameImpl game, Position p) {
         if (game.getUnitAt(p).getTypeString().equals(GameConstants.CARAVAN) && game.getCityAt(p) != null) {
             game.addPopulation(p, 2);
             game.removeUnit(p);
+        } else {
+            gammaCivActionStrategy.unitAction(game, p);
         }
     }
 }
