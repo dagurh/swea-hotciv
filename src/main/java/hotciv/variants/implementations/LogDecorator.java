194620caa8e4e2b0487a095a26614967fdaf0365
Dagur Hardarson <201810188@post.au.dk>
2020-11-05 10:43:34 +0100

    LogDecorator

---
+++ src/main/java/hotciv/variants/implementations/LogDecorator.java
@@ -0,0 +1,77 @@
+package hotciv.variants.implementations;
+
+import hotciv.framework.*;
+import hotciv.standard.GameImpl;
+import hotciv.variants.factories.AlphaCivFactory;
+
+public class LogDecorator implements Game {
+
+    private Game game;
+
+    public LogDecorator (){
+        game = new GameImpl(new AlphaCivFactory());
+    }
+
+    @Override
+    public String getTileAt(Position p) {
+        return game.getTileAt(p);
+    }
+
+    @Override
+    public Unit getUnitAt(Position p) {
+        return game.getUnitAt(p);
+    }
+
+    @Override
+    public City getCityAt(Position p) {
+        return game.getCityAt(p);
+    }
+
+    @Override
+    public Player getPlayerInTurn() {
+        System.out.println("It is " + game.getPlayerInTurn() + "'s turn.");
+        return game.getPlayerInTurn();
+    }
+
+    @Override
+    public Object getWinner() {
+        System.out.println("The winner is " + game.getWinner() + ".");
+        return game.getWinner();
+    }
+
+    @Override
+    public int getAge() {
+        System.out.println("The current age is " + game.getAge() + ".");
+        return game.getAge();
+    }
+
+    @Override
+    public boolean moveUnit(Position from, Position to) {
+        System.out.println(game.getPlayerInTurn() + " moves " + game.getUnitAt(from).getTypeString() + " from " + from + " to " + to + ".");
+        return game.moveUnit(from, to);
+    }
+
+    @Override
+    public void endOfTurn() {
+        System.out.println(game.getPlayerInTurn() + " ends turn.");
+        game.endOfTurn();
+    }
+
+    @Override
+    public void changeWorkForceFocusInCityAt(Position p, String balance) {
+        System.out.println(game.getPlayerInTurn() + " changes work force focus in city at " + p + " to " + balance + " focus.");
+        game.changeWorkForceFocusInCityAt(p, balance);
+    }
+
+    @Override
+    public void changeProductionInCityAt(Position p, String unitType) {
+        System.out.println(game.getPlayerInTurn() + " changes production in  a city at " + p + " to " + unitType + ".");
+        game.changeProductionInCityAt(p, unitType);
+    }
+
+    @Override
+    public void performUnitActionAt(Position p) {
+        System.out.println(game.getPlayerInTurn() + "'s " + game.getUnitAt(p).getTypeString() + " performs an action.");
+        game.performUnitActionAt(p);
+    }
+}
