d591d47c3285c8ef56327083b1eff4da9a6f0518
Jonas <jonasvilla34@gmail.com>
2020-11-12 11:32:52 +0100

    Kinda fake it tests

--- src/test/java/hotciv/variants/TestObserverSpy.java
+++ src/test/java/hotciv/variants/TestObserverSpy.java
@@ -19,22 +19,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestObserverSpy {
 
     private GameImpl game;
-    private Position pos0_7, pos3_8, pos5_5, pos6_5, pos8_12, pos9_6, pos9_7, pos9_8, pos9_12;
+    private Position pos0_1, pos1_0, pos1_1, pos4_1, pos2_0, pos2_1, pos2_2, pos3_1, pos3_2, pos4_2, pos4_3, pos4_4;
     private ObserverSpy observer;
 
     @BeforeEach
     void setUp() {
         game = new GameImpl(new AlphaCivFactory());
         observer = new ObserverSpy();
-        pos0_7 = new Position(0, 7);
-        pos3_8 = new Position(3,8);
-        pos5_5 = new Position(5, 5);
-        pos6_5 = new Position(6, 5);
-        pos8_12 = new Position(8,12);
-        pos9_6 = new Position(9, 6);
-        pos9_7 = new Position(9, 7);
-        pos9_8 = new Position(9, 8);
-        pos9_12 = new Position(9,12);
+        pos0_1 = new Position(0,1);
+        pos1_0 = new Position(1,0);
+        pos1_1 = new Position(1,1);
+        pos2_0 = new Position(2,0);
+        pos2_1 = new Position(2,1);
+        pos2_2 = new Position(2,2);
+        pos3_1 = new Position(3,1);
+        pos3_2 = new Position(3,2);
+        pos4_1 = new Position(4,1);
+        pos4_2 = new Position(4,2);
+        pos4_3 = new Position(4,3);
+        pos4_4 = new Position(4,4);
     }
 
     public void callEndOfTurn(int x) {
@@ -50,6 +53,23 @@ class TestObserverSpy {
         assertThat(observer.getPlayerInTurn(), is(Player.BLUE));
     }
 
+    @Test
+    public void ageIsIncrementedByA100YearsEachTurn(){
+        callEndOfTurn(2);
+        assertThat(observer.getCurrentAge(), is(-3900));
+    }
 
+    @Test
+    public void WorldMapChangesWhenUnitMoves(){
+        game.moveUnit(pos4_3,pos4_4);
+        observer.worldChangedAt(pos4_4);
+        assertThat(observer.getCurrentPosition(), is(pos4_4));
+    }
+
+    @Test
+    public void WorldChangesWhenTileIsSelected(){
+        observer.tileFocusChangedAt(pos4_3);
+        assertThat(observer.getCurrentPosition(), is(pos4_3));
+    }
 
 }
