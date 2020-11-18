698d9a5971c5008962a0e632df02c6d4f35ee1a3
Jonas <jonasvilla34@gmail.com>
2020-11-18 13:54:18 +0100

    showsemi

---
+++ src/main/java/hotciv/visual/NotDefinedYet.java
@@ -0,0 +1,48 @@
+package hotciv.visual;
+
+import hotciv.standard.GameImpl;
+import hotciv.variants.factories.SemiCivFactory;
+import hotciv.view.tool.CompositionTool;
+import hotciv.view.tool.MoveUnitTool;
+import minidraw.standard.*;
+import minidraw.framework.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import hotciv.framework.*;
+import hotciv.view.*;
+import hotciv.stub.*;
+
+/** Template code for exercise FRS 36.39.
+
+ This source code is from the book
+ "Flexible, Reliable Software:
+ Using Patterns and Agile Development"
+ published 2010 by CRC Press.
+ Author:
+ Henrik B Christensen
+ Computer Science Department
+ Aarhus University
+
+ This source code is provided WITHOUT ANY WARRANTY either
+ expressed or implied. You may study, use, modify, and
+ distribute it for non-commercial purposes. For any
+ commercial use, see http://www.baerbak.com/
+ */
+public class NotDefinedYet {
+
+    public static void main(String[] args) {
+        Game game = new GameImpl(new SemiCivFactory());
+
+        DrawingEditor editor =
+                new MiniDrawApplication( "Move any unit using the mouse",
+                        new HotCivFactory4(game) );
+        editor.open();
+        editor.showStatus("Move units to see Game's moveUnit method being called.");
+
+        // TODO: Replace the setting of the tool with your UnitMoveTool implementation.
+        editor.setTool( new CompositionTool(editor, game) );
+    }
+}
